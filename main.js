b527ac2c4ae4a7b4b2d937d0fd1937529286df85
Pedro H. K. Dagostini <79208180+PedroHKD@users.noreply.github.com>
2023-09-12 15:18:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 function criptografar(arquivoTxt, chave){
     const textoI = fs.readFileSync(arquivoTxt, 'utf8')
     let textoC = ''
+    let arquivoTxtFinal = arquivoTxt + '_criptografado.txt'
 
     for (let i = 0; i < textoI.length; i++){
         const cIncial = textoI.charCodeAt(i)
@@ -17,9 +18,10 @@ function criptografar(arquivoTxt, chave){
 }
 
 //funcao para desciptografar
-function descriptografar(arquivoTxt, chave){
+function decriptografar(arquivoTxt, chave){
     const textoI = fs.readFileSync(arquivoTxt, 'utf8')
     let textoD = ''
+    let arquivoTxtFinal = arquivoTxt + '_decriptografado.txt'
 
     for (let i = 0; i < textoI.length; i++){
         const cIncial = textoI.charCodeAt(i)
@@ -31,16 +33,15 @@ function descriptografar(arquivoTxt, chave){
     fs.writeFileSync(arquivoTxtFinal, textoD, 'utf-8')
 }
 
-const arquivoTxt = 'arquivo_final.txt' //coloque aqui o nome do arquivo txt para ser criptografado ou descriptografado
+const arquivoTxt = 'arquivo_inicial.txt_criptografado.txt' //coloque aqui o nome do arquivo txt para ser criptografado ou descriptografado
 const chave = 'chave123' //coloque aqui a chave de criptografia
-const arquivoTxtFinal = 'arquivo_finalDesc.txt' //coloque aqui o nome que deseja para o arquivo
-let opcao = 1 //opcao = 1 para criptografar e opcao = 2 para decriptografar
+const opcao = 'decriptografar' //opcao para criptografar ou decriptografar o arquivo txt
 
-if(opcao = 1){
+if(opcao === 'criptografar'){
     criptografar(arquivoTxt, chave)
     console.log("Arquivo criptografado com sucesso")
-} else if(opcao = 2){
-    descriptografar(arquivoTxt, chave)
+} else if(opcao === 'decriptografar'){
+    decriptografar(arquivoTxt, chave)
     console.log("Arquivo decriptografado com sucesso")
 } else {
     console.log("Opcao invalida")
