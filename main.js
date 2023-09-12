8539d57919ec95be375759b036065c1dccc2f4af
Pedro H. K. Dagostini <79208180+PedroHKD@users.noreply.github.com>
2023-09-12 15:39:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -33,9 +33,15 @@ function decriptografar(arquivoTxt, chave){
     fs.writeFileSync(arquivoTxtFinal, textoD, 'utf-8')
 }
 
-const arquivoTxt = 'arquivo_inicial.txt_criptografado.txt' //coloque aqui o nome do arquivo txt para ser criptografado ou descriptografado
-const chave = 'chave123' //coloque aqui a chave de criptografia
-const opcao = 'decriptografar' //opcao para criptografar ou decriptografar o arquivo txt
+//const arquivoTxt = 'arquivo_inicial.txt_criptografado.txt' //coloque aqui o nome do arquivo txt para ser criptografado ou descriptografado
+//const chave = 'chave123' //coloque aqui a chave de criptografia
+//const opcao = 'decriptografar' //opcao para criptografar ou decriptografar o arquivo txt
+const [arquivoTxt, chave, opcao] = process.argv.slice(2) //Pegar os nomes apartir do terminal
+
+if (!arquivoTxt || !chave || !opcao) {
+    console.log('O script deve ser executado da seguinte forma:\nVa ate a pasta onde o script e crie ou cole o arquivo.txt que voce deseja criptografar/decriptogravar(Vernam)\nApos isso abra o terminal execute o seguinte comando: node main.js nomeDoArquivo chave criptografar ou decriptografar\nExemplo:\n"node main.js aula7.txt questoes criptografar"')
+    process.exit(1)
+}
 
 if(opcao === 'criptografar'){
     criptografar(arquivoTxt, chave)
